3c9e69bfba39eca15bb58c83c06d18770a45ea18
jaimergp <jaimergp@users.noreply.github.com>
2025-01-15 20:30:57 +0100

    use fixture in all tests

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -6,83 +6,83 @@ from conda_spawn.shell import PosixShell, PowershellShell, CmdExeShell
 
 from subprocess import PIPE, check_output
 
+@pytest.fixture(scope="session")
+def simple_env(session_tmp_env):
+    with session_tmp_env("ca-certificates", "--quiet") as prefix:
+        yield prefix
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Pty's only available on Unix")
-def test_posix_shell():
-    proc = PosixShell(sys.prefix).spawn_tty()
+def test_posix_shell(simple_env):
+    proc = PosixShell(simple_env).spawn_tty()
     proc.sendline("env")
     proc.expect("CONDA_SPAWN")
     proc.sendline("echo $CONDA_PREFIX")
-    proc.expect(sys.prefix)
+    proc.expect(str(simple_env))
     proc.kill(signal.SIGINT)
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Powershell only tested on Windows")
-def test_powershell():
-    shell = PowershellShell(sys.prefix)
+def test_powershell(simple_env):
+    shell = PowershellShell(simple_env)
     with shell.spawn_popen(command=["ls", "env:"], stdout=PIPE, text=True) as proc:
         out, _ = proc.communicate()
         proc.kill()
         assert not proc.poll()
         assert "CONDA_SPAWN" in out
         assert "CONDA_PREFIX" in out
+        assert str(simple_env) in out
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Cmd.exe only tested on Windows")
-def test_cmd():
-    shell = CmdExeShell(sys.prefix)
+def test_cmd(simple_env):
+    shell = CmdExeShell(simple_env)
     with shell.spawn_popen(command=["@SET"], stdout=PIPE, text=True) as proc:
         out, _ = proc.communicate()
         proc.kill()
         assert not proc.poll()
         assert "CONDA_SPAWN" in out
         assert "CONDA_PREFIX" in out
+        assert str(simple_env) in out
 
 
-def test_hooks(conda_cli):
-    out, err, rc = conda_cli("spawn", "--hook", "-p", sys.prefix)
+def test_hooks(conda_cli, simple_env):
+    out, err, rc = conda_cli("spawn", "--hook", "-p", simple_env)
     print(out)
     print(err, file=sys.stderr)
     assert not rc
     assert not err
     assert "CONDA_EXE" in out
-    assert sys.prefix in out
-
-
-@pytest.fixture(scope="session")
-def ca_certificates_env(session_tmp_env):
-    with session_tmp_env("ca-certificates", "--quiet") as prefix:
-        yield prefix
+    assert str(simple_env) in out
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Only tested on Unix")
-def test_hooks_integration_posix(ca_certificates_env, tmp_path):
-    hook = f"{sys.executable} -m conda spawn --hook --shell posix -p '{ca_certificates_env}'"
+def test_hooks_integration_posix(simple_env, tmp_path):
+    hook = f"{sys.executable} -m conda spawn --hook --shell posix -p '{simple_env}'"
     script = f'eval "$({hook})"\nenv | sort'
     script_path = tmp_path / "script-eval.sh"
     script_path.write_text(script)
 
     out = check_output(["bash", script_path], text=True)
     print(out)
-    assert str(ca_certificates_env) in out
+    assert str(simple_env) in out
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Powershell only tested on Windows")
-def test_hooks_integration_powershell(ca_certificates_env, tmp_path):
-    hook = f"{sys.executable} -m conda spawn --hook --shell powershell -p {ca_certificates_env}"
+def test_hooks_integration_powershell(simple_env, tmp_path):
+    hook = f"{sys.executable} -m conda spawn --hook --shell powershell -p {simple_env}"
     script = f"{hook} | Out-String | Invoke-Expression\r\nls env:"
     script_path = tmp_path / "script-eval.ps1"
     script_path.write_text(script)
 
     out = check_output(["powershell", "-NoLogo", "-File", script_path], text=True)
     print(out)
-    assert str(ca_certificates_env) in out
+    assert str(simple_env) in out
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Cmd.exe only tested on Windows")
-def test_hooks_integration_cmd(ca_certificates_env, tmp_path):
+def test_hooks_integration_cmd(simple_env, tmp_path):
     hook = (
-        f"{sys.executable} -m conda spawn --hook --shell cmd -p {ca_certificates_env}"
+        f"{sys.executable} -m conda spawn --hook --shell cmd -p {simple_env}"
     )
     script = f"FOR /F \"tokens=*\" %%g IN ('{hook}') do @CALL %%g\r\nset"
     script_path = tmp_path / "script-eval.bat"
@@ -90,4 +90,4 @@ def test_hooks_integration_cmd(ca_certificates_env, tmp_path):
 
     out = check_output(["cmd", "/D", "/C", script_path], text=True)
     print(out)
-    assert str(ca_certificates_env) in out
+    assert str(simple_env) in out
